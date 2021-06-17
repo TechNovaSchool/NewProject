01147ee7f224f9f46ab1e06b87bcff35aef355ef
andrewnova <tech.easy1@outlook.com>
2021-06-17 18:50:10 -0500

    first commit

---
+++ NewProjectTest/src/TestGame1.java
@@ -0,0 +1,5 @@
+public class TestGame1 {
+    public static void main(String[] args) {
+        System.out.printf("Game1");
+    }
+}
---
+++ NewProjectTest/src/TestGame2.java
@@ -0,0 +1,5 @@
+public class TestGame2 {
+    public static void main(String[] args) {
+        System.out.printf("TestGame2");
+    }
+}
